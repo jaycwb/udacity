73acc9bdae2abad63f1d9ee03f93c08917595ede
José Ferraz Neto <netoferraz@gmail.com>
2017-06-22 10:50:13 -0300

    ajustando alinhamento do cabeçalho

--- nanodegree/data_analyst/06_faca_visualizacao_de_dados_eficazes/js/funcoes.js
+++ nanodegree/data_analyst/06_faca_visualizacao_de_dados_eficazes/js/funcoes.js
@@ -367,7 +367,7 @@ function dehighlight(data){
 function moveLabel() {
 
     var x = d3.event.clientX+10; //horizontal label coordinate
-    var y = d3.event.clientY-25; //vertical label coordinate
+    var y = d3.event.clientY-10; //vertical label coordinate
 		
     d3.select(".legenda") //select the label div for moving
         .style("margin-left", x+"px") //reposition label horizontal
