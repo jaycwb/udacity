78fa816eb9fe995b5cb28421c636defc330c12fc
José Ferraz Neto <netoferraz@gmail.com>
2017-06-26 23:35:01 -0300

    organizando os dados json e producao de notebook com kernel R para plotagem do diagrama de sankey

--- nanodegree/data_analyst/06_faca_visualizacao_de_dados_eficazes/scripts/sankey.R
+++ nanodegree/data_analyst/06_faca_visualizacao_de_dados_eficazes/scripts/sankey.R
@@ -1,8 +1,8 @@
 library(networkD3)
+library(jsonlite)
 
-Energy <- jsonlite::fromJSON("C:\\Users\\jferraz\\Desktop\\vendas_olac_agg.json")
-# Plot
+Energy <- jsonlite::fromJSON("C:\\Users\\ferraz\\Documents\\Nanodegree\\Projetos\\nanodegree\\data_analyst\\06_faca_visualizacao_de_dados_eficazes\\data\\vendas_olac_agg.json")
+
 sankeyNetwork(Links = Energy$links, Nodes = Energy$nodes, Source = "source",
               Target = "target", Value = "value", NodeID = "name",
-              units = "TWh", fontSize = 12, nodeWidth = 30)
-
+              units = "TWh", fontSize = 12, nodeWidth = 30, iframe=TRUE)
